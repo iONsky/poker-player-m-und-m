77e9a7dffd5feba3c6dec8ef35d22c1f7f1f3715
Maximilian Wagner <maximilian.wagner@datev.de>
2016-12-22 10:39:02 +0100

    Move

--- Player.js
+++ Player.js
@@ -4,14 +4,14 @@ class Player {
   }
 
   static betRequest(gameState) {
-    var bet = gameState.current_buy_in;
+    var bet = gameState.current_buy_in + gameState.minimum_raise;
     console.log("player " + gameState.players[0].id + gameState.players[0].name);
 
     var cardOne = gameState.players[0].hole_cards[0];
     var cardTwo = gameState.players[0].hole_cards[1];
 
-    console.log("Rank 1" + cardOne.rank);
-    console.log("Rank 2" + cardTwo.rank);
+    console.log("Rank 1 - " + cardOne.rank);
+    console.log("Rank 2 - " + cardTwo.rank);
 
     if(cardOne.rank === "A" || cardOne.rank === "K" || cardOne.rank === "Q" || cardOne.rank === "J" || cardOne.rank === "10") {
       bet += 10;
