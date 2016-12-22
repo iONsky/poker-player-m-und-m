40d407abc30b711c7416b421d43fdae3de52d58e
Maximilian Wagner <maximilian.wagner@datev.de>
2016-12-22 10:06:38 +0100

    Move

--- Player.js
+++ Player.js
@@ -7,9 +7,10 @@ class Player {
     for (var player in gameState.players) {
       if(player.name === "M und M") {
         for (var card in player.hole_cards) {
-            if(card.rank === "A" || card.rank === "K" || card.rank === "Q" || card.rank === "J" || card.rank === "10") {
+          return gameState.current_buy_in + 23;
+            /*if(card.rank === "A" || card.rank === "K" || card.rank === "Q" || card.rank === "J" || card.rank === "10") {
               return gameState.current_buy_in;
-            }
+            }*/
         }
       }
     }
