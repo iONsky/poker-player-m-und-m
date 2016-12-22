0f9fb65af07f2e7f2b63ba7c17f5940320825caf
Maximilian Wagner <maximilian.wagner@datev.de>
2016-12-22 11:23:40 +0100

    Move

--- Player.js
+++ Player.js
@@ -4,7 +4,7 @@ class Player {
   }
 
   static betRequest(gameState) {
-    var bet = gameState.current_buy_in;
+    var bet = 0;
     console.log("player " + gameState.players[0].id + gameState.players[0].name);
     console.log("my bet" + gameState.players[0].bet);
     var cardOne = gameState.players[0].hole_cards[0];
@@ -13,19 +13,22 @@ class Player {
     console.log("Rank 1 - " + cardOne.rank);
     console.log("Rank 2 - " + cardTwo.rank);
 
+    var hasOneHighCard = false
+
+
     if(cardOne.rank === "A" || cardOne.rank === "K" || cardOne.rank === "Q" || cardOne.rank === "J" || cardOne.rank === "10") {
-      bet += gameState.minimum_raise;
+      hasOneHighCard = true
     }
 
     if(cardTwo.rank === "A" || cardTwo.rank === "K" || cardTwo.rank === "Q" || cardTwo.rank === "J" || cardTwo.rank === "10") {
-      bet += gameState.minimum_raise;
+      hasOneHighCard = true
     }
 
-    for (var card in gameState.players[0].hole_cards) {
-      console.log("Card " + card);
+    if(hasOneHighCard) {
+      bet += gameState.current_buy_in;
     }
+
     console.log("Bet " + bet);
-    console.log("new Bet " + gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise)
     return bet;//gameState.current_buy_in - gameState.players[gameState.in_action]["bet"] + gameState.minimum_raise;
   }
 
