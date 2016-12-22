440481a797487a81e7ed50ca2fb9cd33a517dba9
Maximilian Wagner <maximilian.wagner@datev.de>
2016-12-22 13:28:04 +0100

    Fix

--- Player.js
+++ Player.js
@@ -17,8 +17,9 @@ class Player {
     console.log("Rank 1 - " + cardOne.rank);
     console.log("Rank 2 - " + cardTwo.rank);
     console.log("CC " + gameState.community_cards.length);
-    for (var cc in gameState.community_cards) {
-      console.log("CC _ " + cc);
+    for (var index in gameState.community_cards) {
+      var card = gameState.community_cards[index];
+      console.log("CC _ " + card.rank);
     }
 
     var value = Player.checkCardsRank(cardOne, cardTwo);
